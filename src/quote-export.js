3a7db57bde8f449de1d1a2e058c4f53dbcffa2e9
John Miller <john2021@gmail.com>
2014-12-16 21:01:35 -0500

    exports results to csv in reverse order

--- src/quote-export.js
+++ src/quote-export.js
@@ -2,9 +2,10 @@ var fs = require('fs');
 var csv = require('csv');
 
 function writeFile(data, path){
-	csv.stringify(data, {header: true}, function(err,  output){
-		fs.writeFile(path, output, 'UTF-8');
+	csv.stringify(data.reverse(), {header: true}, function(err,  output){
+		fs.writeFileSync(path, output, 'UTF-8');
 	});
 }
+
 
 exports.writeFile = writeFile;
--- test/quote-export-tests.js
+++ test/quote-export-tests.js
@@ -1,17 +1,19 @@
 var should = require('should');
 var fs = require('fs');
-var csv = require('csv');
-var rewire = require('rewire');
-var quoteExport = rewire('../src/quote-export');
+var quoteExport = require('../src/quote-export');
 
 describe('QuoteExport', function(){
-	it('should write results to csv file', function(){
-		var quotes = [{col1: 'some string', col2: '100.12'}];
-		var fileName = __dirname+'/result.csv';
+	it('should write results in reverse order', function(){
+		var quotes = [
+			{col1: 'string1', col2: '100.12'},
+			{col1: 'string2', col2: '200.12'}
+		];
+
+		var fileName = __dirname+'/quote-export-test.csv';
 
 		quoteExport.writeFile(quotes, fileName);
 
 		var result = fs.readFileSync(fileName, 'utf8');
-		result.should.eql('col1,col2\nsome string,100.12\n');
+		result.should.eql('col1,col2\nstring2,200.12\nstring1,100.12\n');
 	});
 });
